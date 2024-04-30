590b3e3bbc027468069dd7eafe202acdfbc3e8a6
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-04-30 20:06:37 +0100

    chore: adding commonjs node16 example

---
+++ examples/ts-commonjs-node16/consumer.ts
@@ -0,0 +1,11 @@
+const { Consumer } = require('sqs-consumer');
+
+const initConsumer = (options) => {
+  console.log('Starting SQS Consumer now...');
+
+  const consumer = new Consumer(options);
+
+  return consumer;
+};
+
+module.exports = initConsumer;
