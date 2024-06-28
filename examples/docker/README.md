73d008826b701fe96cdb57e53b706e1d12ced5ec
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-06-28 15:40:42 +0100

    chore: adding docker example

---
+++ examples/docker/README.md
@@ -0,0 +1,7 @@
+# sqs-consumer-starter
+
+This is a Docker example of how to use the `sqs-consumer` library to consume messages from an SQS queue.
+
+Alongside being able to start the sqs-consumer polling process, it will also start a localstack instance to simulate the SQS queue. This will be made available at `http://localhost:4566`.
+
+[Open in StackBlitz](https://stackblitz.com/github/bbc/sqs-consumer-starter/tree/main/examples/docker).
---
+++ examples/docker/index.ts
@@ -0,0 +1,33 @@
+import express, { static as expressStatic } from 'express';
+import { resolve } from 'node:path';
+import debug from 'debug';
+import { SQSClient } from '@aws-sdk/client-sqs';
+
+import initConsumer from './consumer.js';
+
+const app = express();
+const port = 3010;
+
+app.use(expressStatic('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve('pages/index.html'));
+});
+
+app.listen(port, () => {
+  // Edit these options if you need to
+  const options = {
+    queueUrl: 'http://127.0.0.1:4566/000000000000/sqs-consumer-test',
+    sqs: new SQSClient({
+      endpoint: 'http://127.0.0.1:4566/000000000000/sqs-consumer-test',
+    }),
+    handleMessage: async (msg) => {
+      debug('Handled a message...');
+      debug(msg);
+    },
+  };
+  const consumer = initConsumer(options);
+
+  // Add your use case below, the rest of the setup has already been sorted out for you above.
+  consumer.start();
+});
