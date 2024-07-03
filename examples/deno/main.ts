19cb9b3125f223e181e5fb7d5590d62250d5adf5
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2024-07-03 21:25:40 +0100

    chore: adding deno example

---
+++ examples/deno/main.ts
@@ -0,0 +1,31 @@
+import { Consumer } from '@bbc/sqs-consumer';
+
+const sqs = {
+  send: async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2000);
+    });
+    return [];
+  },
+};
+
+const initConsumer = (options) => {
+  console.log('Starting SQS Consumer now...');
+
+  const consumer = new Consumer(options);
+
+  return consumer;
+};
+
+const options = {
+  queueUrl: 'some-url',
+  sqs,
+  handleMessage: async (msg) => {
+    console.log('Handled a message...');
+    console.log(msg);
+  },
+};
+
+const consumer = initConsumer(options);
+
+consumer.start();
